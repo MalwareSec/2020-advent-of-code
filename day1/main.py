d7f46fe71c1ee27ee1e2b2ef96604e49fd6d4fbe
MalwareSec <dannyc10@vt.edu>
2020-12-01 21:57:50 -0500

    refact

--- day1/main.py
+++ day1/main.py
@@ -1,5 +1,12 @@
-from utils import process_input_to_list
+import timeit
+import statistics
 from typing import List
+
+
+def process_input_to_list(filename: str) -> List[int]:
+    with open(filename) as f:
+        lst = [int(x) for x in f.read().split()]
+    return lst
 
 
 def process_list_two_sum(inputList: List[int],
@@ -25,12 +32,35 @@ def process_list_three_sum(inputList: List[int],
             cache.add(inputList[j])
 
 
-def main():
-    inputList = process_input_to_list('input.txt')
-    two_sum = process_list_two_sum(inputList, 2020)
-    three_sum = process_list_three_sum(inputList, 2020)
-    print(three_sum)
+def testspeed(part: str) -> float:
+    test_map = {
+        'part1': {
+            'setup': '''from __main__ import process_list_two_sum, process_input_to_list\ninputList = process_input_to_list('input.txt')''',
+            'stmt': '''process_list_two_sum(inputList, 2020)'''
+        },
+        'part2': {
+            'setup': '''from __main__ import process_list_three_sum, process_input_to_list\ninputList = process_input_to_list('input.txt')''',
+            'stmt': '''process_list_three_sum(inputList, 2020)'''
+        }
+    }
+    reps = timeit.repeat(repeat = 10,
+                         number = 1,
+                         stmt=test_map[part]["stmt"],
+                         setup=test_map[part]["setup"])
+    return statistics.mean(reps) * 1000
 
+
+def part1(inputList):
+    two_sum = process_list_two_sum(inputList, 2020)
+    speed = testspeed('part1')
+    print('Part One: {} -- {:0.2f} ms'.format(two_sum, speed))
+
+def part2(inputList):
+    three_sum = process_list_three_sum(inputList, 2020)
+    speed = testspeed('part2')
+    print('Part Two: {} -- {:0.2f} ms'.format(three_sum, speed))
 
 if __name__ == '__main__':
-    main()
+    inputList = process_input_to_list('input.txt')
+    part1(inputList)
+    part2(inputList)
--- day1/utils.py
+++
@@ -1,7 +0,0 @@
-from typing import List
-
-
-def process_input_to_list(filename: str) -> List[int]:
-    with open(filename) as f:
-        lst = [int(x) for x in f.read().split()]
-    return lst
